d443728b8311c61b80586aae19bc50019eb44213
Caen De Silva <caen@desilva.se>
2022-06-03 14:07:51 +0200

    Create ResponseInterface.php

---
+++ src/Contracts/ResponseInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Desilva\Microserve\Contracts;
+
+interface ResponseInterface
+{
+    public function __construct(int $statusCode = 200, string $statusMessage = 'OK', array $data = []);
+    public function __toString(): string;
+    public function send(): void;
+}
