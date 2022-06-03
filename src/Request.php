6f9f3c84362f6a0c9c9ad79578fed6d4b1ab3116
Caen De Silva <caen@desilva.se>
2022-06-03 14:08:30 +0200

    Create Request.php

---
+++ src/Request.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Desilva\Microserve;
+
+use JetBrains\PhpStorm\ArrayShape;
+
+class Request
+{
+    public string $method;
+    public string $path;
+    public array $data;
+
+    public function __construct(array $data = [])
+    {
+        $this->data = $data;
+
+        $this->method = $_SERVER['REQUEST_METHOD'];
+        $this->path = urldecode(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+    }
+
+    public static function capture(): Request
+    {
+        return new self($_REQUEST);
+    }
+
+    public function __get(string $key): ?string
+    {
+        return $this->data[$key] ?? null;
+    }
+
+    #[ArrayShape(['method' => "string", 'path' => "string", 'data' => "array"])]
+    public function __serialize(): array
+    {
+        return [
+            'method' => $this->method,
+            'path' => $this->path,
+            'data' => $this->data,
+        ];
+    }
+}
